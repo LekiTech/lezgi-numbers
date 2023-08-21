db9882f052b599af62396ca5e4fbd2ba3b611af6
Kamran Tadzjibov <ktadzjibov@gmail.com>
2023-08-22 00:47:37 +0200

    working numbers converter to the string representation in Lezgi language

---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+export const million = 1e6; // 10^6;
+export const billion = 1e9; // 10^9;
+export const trillion = 1e12; // 10^12;
+export const quadrillion = 1e15; // 10^15;
+export const quintillion = 1e18; // 10^18;
+export const sextillion = 1e21; // 10^21;
+export const septillion = 1e24; // 10^24;
+export const octillion = 1e27; // 10^27;
+export const nonillion = 1e30; // 10^30;
+
+export const atomic = {
+  0: 'нол',
+  1: 'сад',
+  2: 'кьвед',
+  3: 'пуд',
+  4: 'кьуд',
+  5: 'вад',
+  6: 'ругуд',
+  7: 'ирид',
+  8: 'муьжуьд',
+  9: 'кIуьд',
+  10: 'цIуд',
+  20: 'къад',
+  40: 'яхцIур',
+  // 10^2
+  100: 'виш',
+  // 10^3
+  1000: 'агъзур',
+  // 10^6
+  [million]: 'миллион',
+  // 10^9
+  [billion]: 'миллиард',
+  // 10^12
+  [trillion]: 'триллион',
+  // 10^15
+  [quadrillion]: 'квадриллион',
+  // 10^18
+  [quintillion]: 'квинтиллион',
+  // 10^21
+  [sextillion]: 'секстиллион',
+  // 10^24
+  [septillion]: 'септиллион',
+  // 10^27
+  [octillion]: 'октиллион',
+  // 10^30
+  [nonillion]: 'нониллион',
+};
